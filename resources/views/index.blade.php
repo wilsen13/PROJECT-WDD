8c6d5b3d64176ef4cca7b3e44c9fd60eca1f5230
wilsen13 <wilsengoms@gmail.com>
2025-12-09 11:41:29 +0700

    indek

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.main')
-
 @section('title', 'Home - SatuHati')
+@section('keywords', 'SatuHati, Donasi')
 
 @section('content')
+
     <section class="hero">
         <div class="hero-overlay"></div>
         <div class="hero-content">
